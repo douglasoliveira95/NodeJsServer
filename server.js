a22279a01cbeebf370e73fa5570dd61422b89bc0
Douglas Oliveira <douglinhas_uchiha@hotmail.com>
2016-10-13 21:55:42 -0300

    SecondCommit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const http = require('http');
 const server = http.createServer(function onRequest(request, response) {
+    console.log('Request for: '+ request.url);
+    console.log('From:'+ request.ip);
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
     response.end('Hello World');
