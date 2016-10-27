ce5d0f168727a6cbe94449405af5901ddc141232
Douglas Oliveira <douglinhas_uchiha@hotmail.com>
2016-10-27 21:41:08 -0200

    servidorHTML

---
+++ router.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function route(path) {
+    console.log("Route to " + path)    
+}
+
+//Public API
+exports.route = route
+
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 "use strict"
 
-const http = require('http');
-const server = http.createServer(function onRequest(request, response) {
-    console.log('Request for: '+ request.url);
-    console.log('From:'+ request.ip);
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World');
-});
+const http = require('http')
+const host = 'localhost'
+const port = 9000
+const url = require('url');
 
-server.listen('9000', function started() {
-    console.log('Server started at http://localhost:9000');
-}) 
+function start(route) {
+   http
+    .createServer(onRequest)
+    .listen(port, onStart) 
     
+    function onRequest(request, response) {
+    console.log('Request for: ' + request.url)
+    
+    let path = url.parse(request.url).path
+    route(path)
+    
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/plain')
+    response.end('Hello World')
+    }
+}
+
+function onStart() {
+    console.log(`Server started at http://${host}:${port}`)
+}
+
+//Public API
+exports.start = start
